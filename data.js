9bf3f505f99cb0bb59b3fbfe0e1a27227f3b5fc6
Pio <pengou22@outlook.com>
2025-08-01 14:29:15 +0800

    添加了最新的行程数据

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 // 记录火车站、高铁站地理位置
 var station = {
+    // rank代表不同级别，rank=0的代表不标注
     // '北京': { latLng: [39.90, 116.41], name: '北京', rank: 1 },
     // '上海': { latLng: [31.24, 121.50], name: '上海', rank: 1 },
     // '重庆': { latLng: [29.56, 106.54], name: '重庆', rank: 1 },
@@ -7,8 +8,8 @@ var station = {
     'hch': { latLng: [29.96, 106.28], name: '合川', rank: 1 },
     'cqb': { latLng: [29.61, 106.55], name: '重庆北', rank: 1 },
     'cqx': { latLng: [29.50, 106.44], name: '重庆西', rank: 1 },
-    'gyd': { latLng: [26.66, 106.74], name: '贵阳东', rank: 0 },
-    'gyb': { latLng: [26.62, 106.67], name: '贵阳北', rank: 0 },
+    'gyd': { latLng: [26.66, 106.74], name: '贵阳东', rank: 1 },
+    'gyb': { latLng: [26.62, 106.67], name: '贵阳北', rank: 1 },
     'gzn': { latLng: [22.99, 113.27], name: '广州南', rank: 1 },
     'szb': { latLng: [22.61, 114.03], name: '深圳北', rank: 1 },
     'lji': { latLng: [26.81, 100.25], name: '丽江', rank: 1 },
@@ -29,6 +30,23 @@ var station = {
     'gyu': { latLng: [32.45, 105.82], name: '广元', rank: 0 },
     'hdp': { latLng: [34.27, 104.23], name: '哈达铺', rank: 0 },
     'lzh': { latLng: [36.03, 103.85], name: '兰州', rank: 0 },
+    'csn': { latLng: [28.15, 113.07], name: '长沙南', rank: 0 },
+    'wha': { latLng: [30.61, 114.42], name: '武汉', rank: 1 },
+    'hfn': { latLng: [31.80, 117.29], name: '合肥南', rank: 1 },
+    'hfe': { latLng: [31.89, 117.32], name: '合肥', rank: 1 },
+    'tch': { latLng: [31.05, 116.98], name: '桐城', rank: 0 },
+    'hni': { latLng: [30.76, 116.82], name: '怀宁', rank: 0 },
+    'jji': { latLng: [29.70, 116.01], name: '九江', rank: 0 },
+    'nch': { latLng: [28.67, 115.92], name: '南昌', rank: 0 },
+    'gzh': { latLng: [25.82, 114.96], name: '赣州', rank: 0 },
+    'lch': { latLng: [24.11, 115.24], name: '龙川', rank: 0 },
+    'hyu': { latLng: [23.76, 114.68], name: '河源', rank: 1 },
+    'hyd': { latLng: [23.68, 114.74], name: '河源东', rank: 1 },
+    'hzh': { latLng: [23.15, 114.42], name: '惠州', rank: 0 },
+    'dgd': { latLng: [22.97, 114.04], name: '东莞东', rank: 0 },
+    'szd': { latLng: [22.60, 114.12], name: '深圳东', rank: 1 },
+    'xgxjl': { latLng: [22.30, 114.17], name: '香港西九龙', rank: 1 },
+    'szs': { latLng: [22.71, 114.33], name: '深圳坪山', rank: 1 },
     // '': { latLng: [, ], name: '', rank: 1 },
     // '': { latLng: [, ], name: '', rank: 0 },
 };
@@ -43,6 +61,41 @@ var lines = [
     //     time: '2024-01-21',
     //     route: []
     // },
+    {
+        information: 'G6037',
+        time: '2025-07-30',
+        route: ['gzn', 'hme', 'szb', 'szs']
+    },
+    {
+        information: 'G6022',
+        time: '2025-07-28',
+        route: ['szb', 'hme', 'gzn']
+    },
+    {
+        information: 'G6022',
+        time: '2025-07-26',
+        route: ['xgxjl', 'szb']
+    },
+    {
+        information: 'G5625',
+        time: '2025-07-26',
+        route: ['szb', 'xgxjl']
+    },
+    {
+        information: 'K91',
+        time: '2025-07-22',
+        route: ['hfe', 'tch', 'hni', 'jji', 'nch', 'gzh', 'lch', 'hyu', 'hzh', 'dgd', 'szd']
+    },
+    {
+        information: 'G600',
+        time: '2025-07-19',
+        route: ['wha', 'hfn']
+    },
+    {
+        information: 'G880',
+        time: '2025-07-15',
+        route: ['szb', 'hme', 'gzn', 'csn', 'wha']
+    },
     {
         information: 'G305',
         time: '2025-04-20',
@@ -71,7 +124,7 @@ var lines = [
     {
         information: 'G3708',
         time: '2025-01-18',
-        route: ['gzn', 'glx', 'gyb']
+        route: ['gzn', 'glx', 'gyd', 'gyb']
     },
     {
         information: 'G2908',
@@ -98,12 +151,12 @@ var lines = [
     {
         information: 'G2976',
         time: '2024-09-13',
-        route: ['szb', 'hme', 'gzn', 'glx', 'gyb', 'kmi']
+        route: ['szb', 'hme', 'gzn', 'glx', 'gyd', 'gyb', 'kmi']
     },
     {
         information: 'G2965',
         time: '2024-08-24',
-        route: ['cqx', 'zyi', 'gyb', 'glx', 'gzn', 'hme', 'szb']
+        route: ['cqx', 'zyi', 'gyd', 'glx', 'gzn', 'hme', 'szb']
     },
     {
         information: 'D5160',
@@ -141,17 +194,17 @@ var lines = [
     {
         information: 'G2944',
         time: '2024-07-18',
-        route: ['szb', 'hme', 'gzn', 'glx', 'gyb', 'zyi', 'cqx']
+        route: ['szb', 'hme', 'gzn', 'glx', 'gyd', 'zyi', 'cqx']
     },
     {
         information: 'G2943',
         time: '2024-03-02',
-        route: ['cqx', 'zyi', 'gyb', 'glx', 'gzn', 'hme', 'szb']
+        route: ['cqx', 'zyi', 'gyd', 'glx', 'gzn', 'hme', 'szb']
     },
     {
         information: 'G2942',
         time: '2024-02-07',
-        route: ['szb', 'hme', 'gzn', 'glx', 'gyb', 'zyi', 'cqx']
+        route: ['szb', 'hme', 'gzn', 'glx', 'gyd', 'zyi', 'cqx']
     },
     {
         information: 'G1021',
@@ -161,6 +214,36 @@ var lines = [
     {
         information: 'G6582',
         time: '2024-01-18',
+        route: ['szb', 'hme', 'gzn']
+    },
+    {
+        information: 'G5608',
+        time: '2023-11-18',
+        route: ['xgxjl', 'szb']
+    },
+    {
+        information: 'G5615',
+        time: '2023-11-18',
+        route: ['szb', 'xgxjl']
+    },
+    {
+        information: 'G2709',
+        time: '2023-07-09',
+        route: ['hyd', 'szb']
+    },
+    {
+        information: 'K446',
+        time: '2023-07-08',
+        route: ['szd', 'dgd', 'hzh', 'hyu']
+    },
+    {
+        information: 'G2903',
+        time: '2023-04-15',
+        route: ['gzn', 'hme', 'sxb']
+    },
+    {
+        information: 'G6510',
+        time: '2023-04-14',
         route: ['szb', 'hme', 'gzn']
     },
     // {
